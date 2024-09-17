fd50167c934ed3f7f4c36be299b4c7ae58b1e0f1
Ben Holmes <ben.holmes@hashicorp.com>
2024-09-17 10:03:27 +0100

    refactored for ns

--- policies.tf
+++ policies.tf
@@ -2,11 +2,11 @@ resource "vault_policy" "azure-policy" {
   name = "standard-policy"
 
   policy = <<EOT
-path "${vault_namespace.tenant.path}/azure/*" {
+path "${vault_namespace.tenant.path_fq}/azure/*" {
   capabilities = ["read","create","list","update"]
 }
 
-path "${vault_namespace.tenant.path}/kvv2/+/{{identity.entity.name}}/*" {
+path "${vault_namespace.tenant.path_fq}/kvv2/+/{{identity.entity.name}}/*" {
     capabilities = [ "create", "update", "read", "delete", "list" ]
 }
 EOT
